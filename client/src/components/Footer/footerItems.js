30407919318acdc890ec350d99b37a90affca6fb
Brarian <b.namonywa@gmail.com>
2018-10-25 20:42:07 -0500

    Footer started, needs images and styling

---
+++ client/src/components/Footer/footerItems.js
@@ -0,0 +1,40 @@
+export const footerItems = [
+ {
+  text: "Copyright 2018"
+ },
+ {
+  link: "#",
+  text: "Pios Labs",
+  src: null
+ },
+ {
+  link: "#",
+  text: "Privacy Policy",
+  src: null
+ },
+ {
+  link: "#",
+  text: "Subscribe",
+  src: null
+ },
+ {
+  link: "https://www.nba.com/rockets/",
+  text: null,
+  src: "#, Patreon"
+ }, 
+ { 
+  link: "https://www.nba.com/rockets/",
+  text: null,
+  src: "#, SoundCloud"
+ }, 
+ {
+  link: "https://www.nba.com/rockets/",
+  text: null,
+  src: "#, Facebook"
+ }, 
+ {
+  link: "https://www.nba.com/rockets/",
+  text: null,
+  src: "#, Twitter"
+ }
+];
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { footerItems } from "./footerItems.js";
+
+const Footer = () => {
+ const FooterListItems = footerItems.map((item, i) => {
+  return (
+   <li key={i}>
+    <a href={item.link}>{item.text} <img src={item.src} /></a>
+   </li>
+  );
+ });
+
+  return (
+   <div>
+    <ul>
+     {FooterListItems}
+    </ul>
+   </div>
+  );
+}
+
+export default Footer;
