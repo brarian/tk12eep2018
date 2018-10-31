873edf5ce9fd20b80dee1b9013973c5ede4a02f2
Brarian <b.namonywa@gmail.com>
2018-10-31 18:26:16 -0500

    small changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,30 +1,35 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from './components/Header/header'
-import NotFound from "./components/NotFound/NotFound";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Header from './components/Header/header'
+import Footer from "./components/Footer";
+// import NotFound from "./components/NotFound/NotFound";
 import registerServiceWorker from './registerServiceWorker';
 //import Header from "client/src/components/Header";
 
-class ErrorPage extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <NotFound />
-      </div>
-    );
-  }
-}
+// class ErrorPage extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Header />
+//         <NotFound />
+//       </div>
+//     );
+//   }
+// }
 
 const Site = () => {
   return (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Header} />
-      <Route  component={ErrorPage} />
-    </Switch>
-  </Router>
+  // <Router>
+  //   <Switch>
+  //     <Route exact path="/" component={Header} />
+  //     <Route  component={ErrorPage} />
+  //   </Switch>
+  // </Router>
+  <div>
+  <Footer />
+
+  </div>
   );
 }
 
